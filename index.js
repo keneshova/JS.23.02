00920c90b9a8ca1ef0dc9b87395ee18689daaea7
Nestan <nestankeneshova16@gmail.com>
2023-02-23 12:41:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let example1 = document.querySelector("p");
+example1.textContent  = " 🎉" + example1.textContent + " 🎂";
+console.log(example1);
+
+let example2 = document.querySelector(".example-class");
+example2.style.backgroundColor = "tomato";
+example2.style.color = "white";
+console.log(example2);
+
+
+let example3 = document.querySelector("#example-id");
+example3.style.backgroundImage = "url(/anime.jpg)";
+
+let example4 = document.querySelector("p:nth-child(4)");
+
+example4.
